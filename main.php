cbb65c8ebb1d829b5a284743d0d0cc0e19013aa7
ArielMAJ <ariel.maj@hotmail.com>
2023-04-16 12:55:27 -0300

    [UPT][Task-03] Update solution from functional to OOP.

--- main.php
+++ main.php
@@ -1,50 +1,69 @@
 <?php
-
-function draw_cross()
+abstract class ObjectDrawer
 {
-  echo "Cross:\n\n";
-  for ($x = 0; $x < 5; $x++) {
-    if ($x != 1) {
-      echo "...*...";
-    } else {
-      echo ".*****.";
+  abstract public static function draw();
+}
+
+class CrossDrawer extends ObjectDrawer
+{
+  public static function draw()
+  {
+    echo "Cross:\n\n";
+    for ($x = 0; $x < 5; $x++) {
+      if ($x != 1) {
+        echo "...*...";
+      } else {
+        echo ".*****.";
+      }
+      echo "\n";
     }
-    echo "\n";
   }
 }
 
-function draw_x()
+class XDrawer extends ObjectDrawer
 {
-  echo "X:\n\n";
-  for ($x = 0; $x < 5; $x++) {
-    if ($x == 0 || $x == 4) {
-      echo "*...*";
-    } else if ($x == 2) {
-      echo "..*..";
-    } else {
-      echo ".*.*.";
+  public static function draw()
+  {
+    echo "X:\n\n";
+    for ($x = 0; $x < 5; $x++) {
+      if ($x == 0 || $x == 4) {
+        echo "*...*";
+      } else if ($x == 2) {
+        echo "..*..";
+      } else {
+        echo ".*.*.";
+      }
+      echo "\n";
     }
-    echo "\n";
   }
 }
 
-function main()
+class Main
 {
-  while (true) {
-    $input = readline("C or X? ");
-    if ($input == "C" || $input == "c") {
-      draw_cross();
-    } else if ($input == "X" || $input == "x") {
-      draw_x();
-    } else {
-      echo "Invalid input\n";
+  public static function main()
+  {
+    while (true) {
+      $input = readline("C or X? ");
+      if ($input == "C" || $input == "c") {
+        CrossDrawer::draw();
+      } else if ($input == "X" || $input == "x") {
+        XDrawer::draw();
+      } else {
+        echo "Invalid input\n";
+      }
+      echo "\n";
+      readline("Ctrl+C to exit, enter to continue.");
+      self::clearScreen();
     }
-    echo "\n";
-    readline("Ctrl+C to exit, enter to continue.");
+  }
+
+  private static function clearScreen()
+  {
     echo chr(27) . chr(91) . 'H' . chr(27) . chr(91) . 'J'; //^[H^[J  // Clear screen
   }
 }
 
-main();
+
+Main::main();
 
 ?>
